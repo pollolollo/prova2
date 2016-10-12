33915474fda88e15613b082d89c887bdd78b3fcb
pollolollo <iofrancescoloro@virgilio.it>
2016-10-12 23:50:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/portatilelenovo/prova2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.portatilelenovo.prova2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //prova a caricare solo questo file
+    }
+}
